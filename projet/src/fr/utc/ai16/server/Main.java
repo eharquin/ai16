08e25a19c4d17fc6c80652dd8a53b4c5be1718b6
Matthieu SOLAN <matthieu.solan@etu.utc.fr>
2022-03-09 09:40:38 +0100

    list array

--- projet/src/fr/utc/ai16/server/Main.java
+++ projet/src/fr/utc/ai16/server/Main.java
@@ -6,11 +6,12 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+    ArrayList Clients = new ArrayList(penConnection);
 
     public static void main(String[] args) throws IOException
     {
         ServerSocket serverSocket = new ServerSocket(5056);
-        ArrayList Clients = new ArrayList(OpenConnection);
+
 
 
 
@@ -26,17 +27,11 @@ public class Main {
                 System.out.println("A new client is connected : " + socket);
 
                 ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-                ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
-
-                Clients
+                ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream()
 
                 Thread t = new ClientHandler(Clients, socket, inputStream, outputStream);
 
                 t.start();
-
-
-
-                Clients.add(outputStream);
 
 
 
