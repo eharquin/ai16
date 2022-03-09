8b81006756e16411c32d38304c526fd839184d9d
Antonin <antonin.guyot@icloud.com>
2022-03-09 09:32:29 +0100

    OpenConnection object

---
+++ projet/src/fr/utc/ai16/server/OpenConnection.java
@@ -0,0 +1,13 @@
+package fr.utc.ai16.server;
+
+import java.net.Socket;
+
+public class OpenConnection {
+    String username;
+    Socket socket;
+
+    public OpenConnection(Object content, Socket socket) {
+        this.username = username;
+        this.socket = socket;
+    }
+}
